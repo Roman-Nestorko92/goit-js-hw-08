88fed7b0f507aa20d897ddd534817f7fd6a43afd
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2023-02-02 21:06:53 +0200

    three

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(saveItems, 500));
+form.addEventListener('submit', funcOnSaveItems);
+
+const objItem = {};
+
+function saveItems(e) {
+  objItem[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(objItem));
+}
+
+function funcOnSaveItems(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+(function dataLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+})();
+
 
